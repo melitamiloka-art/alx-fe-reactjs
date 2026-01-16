2e04466dcdd2e4dd6673ccafa07a5bb0840ea55c
Melita <melitamiloka@gmail.com>
2026-01-16 19:29:32 +0100

    A alx-react-app-props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,6 @@
+import React, { useState } from 'react';
 import UserProfile from './components/UserProfile';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import WelcomeMessage from './components/WelcomeMessage';
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import UserContext from './UserContext'; // <-- import the context
-import './App.css';
+import UserContext from './UserContext';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,40 +14,14 @@ function App() {
 
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <WelcomeMessage />
-
       
       <UserContext.Provider value={userData}>
         <UserProfile />
       </UserContext.Provider>
 
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount(count + 1)}>count is {count}</button>
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
