5f799f84eb07854201ddb9eccb1904018466c0de
Melita <melitamiloka@gmail.com>
2026-01-16 19:44:00 +0100

    Add alx-react-app-props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import UserContext from '../UserContext';
 
 function UserProfile() {
-  const userData = useContext(UserContext); 
+  const userData = useContext(UserContext);
 
   return (
     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
