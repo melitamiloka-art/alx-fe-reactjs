1c8373c4f6aca89611be7400b6bbea7293e11c74
Melita <melitamiloka@gmail.com>
2026-01-17 11:18:12 +0100

    Add my-company

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer style={{
+      padding: '10px',
+      textAlign: 'center',
+      backgroundColor: '#f1f1f1'
+    }}>
+      <p>© 2026 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
