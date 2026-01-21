e9919fbffe5fac2283e222bc1dbc802a9e7c4065
Melita <melitamiloka@gmail.com>
2026-01-21 18:57:08 +0100

    Add recipr-sharing-app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,16 @@
-import useRecipeStore from '../store/recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../store/recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      <h2>Recipes</h2>
-      {recipes.length === 0 && <p>No recipes added yet.</p>}
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
+          <h3>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
         </div>
       ))}
     </div>
