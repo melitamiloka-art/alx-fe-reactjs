c26a9063c9122689b4f6531e1f7976ec6e6bca82
Melita <melitamiloka@gmail.com>
2026-01-21 18:02:00 +0100

    Add recipe-sharing-app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing Application</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
