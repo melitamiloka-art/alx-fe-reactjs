a6f2f0230e793ac531f138d02861da0919cb886d
Melita <melitamiloka@gmail.com>
2026-01-26 19:40:28 +0100

    Add recipe-sharing-app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,34 +2,18 @@ import { useRecipeStore } from '../store/recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
-  const favorites = useRecipeStore((state) => state.favorites);
-  const addFavorite = useRecipeStore((state) => state.addFavorite);
-  const removeFavorite = useRecipeStore((state) => state.removeFavorite);
 
   return (
     <div>
-      <h2>All Recipes</h2>
-
-      {recipes.map((recipe) => {
-        const isFavorite = favorites.includes(recipe.id);
-
-        return (
-          <div key={recipe.id}>
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
-
-            <button
-              onClick={() =>
-                isFavorite
-                  ? removeFavorite(recipe.id)
-                  : addFavorite(recipe.id)
-              }
-            >
-              {isFavorite ? 'Remove Favorite ❤️' : 'Add to Favorites 🤍'}
-            </button>
-          </div>
-        );
-      })}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
