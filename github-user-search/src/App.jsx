04764e536206c7974a9f793928daa34e0821a642
Melita <melitamiloka@gmail.com>
2026-01-31 09:07:28 +0100

    Add github-user-search

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,9 @@
+import React from "react";
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Search for GitHub users using the GitHub API.</p>
     </div>
   );
 }
