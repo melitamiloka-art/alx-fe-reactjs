bd42c78ed37096db8f6bf08dd90cc57d2955438c
Melita <melitamiloka@gmail.com>
2026-02-07 17:01:38 +0100

    Add recipe-sharing-app

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,20 +4,22 @@ export const useRecipeStore = create((set, get) => ({
   recipes: [],
   favorites: [],
   recommendations: [],
+  searchTerm: "",
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
 
   setRecipes: (recipes) => set({ recipes }),
 
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
 
-  
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
- 
+
   deleteRecipe: (recipeId) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
