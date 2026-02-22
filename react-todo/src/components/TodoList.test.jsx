a24c8e4e79197f860c2b02385eb2fc830c6c1320
Melita <melitamiloka@gmail.com>
2026-02-22 13:07:03 +0100

    Add react-todo

--- react-todo/src/components/TodoList.test.jsx
+++ react-todo/src/components/TodoList.test.jsx
@@ -1,13 +1,14 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from '../components/TodoList';  
+import TodoList from '../components/TodoList'; 
 
 describe('TodoList Component', () => {
+  
   test('renders initial todos', () => {
     render(<TodoList />);
-    
     expect(screen.getByText('Learn React')).toBeInTheDocument();
     expect(screen.getByText('Build a Todo App')).toBeInTheDocument();
   });
+
 
   test('adds a new todo', () => {
     render(<TodoList />);
@@ -19,19 +20,19 @@ describe('TodoList Component', () => {
     expect(screen.getByText('Test Todo')).toBeInTheDocument();  
   });
 
+  
   test('toggles a todo', () => {
     render(<TodoList />);
     const todo = screen.getByText('Learn React');
     fireEvent.click(todo);  
-
     expect(todo).toHaveStyle('text-decoration: line-through');  
   });
 
+  
   test('deletes a todo', () => {
     render(<TodoList />);
     const todo = screen.getByText('Learn React');
-    const deleteButton = screen.getAllByText('Delete')[0];
-
+    const deleteButton = screen.getAllByText('Delete')[0];  
     fireEvent.click(deleteButton);  
     expect(todo).not.toBeInTheDocument();  
   });
