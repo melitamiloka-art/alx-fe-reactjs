797f9097ee5300059b245109e015ba45eafc0f11
Melita <melitamiloka@gmail.com>
2026-02-22 18:27:04 +0100

    Add react-todo

--- react-todo/src/_test_/Todolist.test.js
+++ react-todo/src/_test_/Todolist.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from '../components/TodoList';  
+import TodoList from '../components/TodoList'; 
+
 describe('TodoList Component', () => {
-  
+
   test('renders initial todos', () => {
     render(<TodoList />);
     expect(screen.getByText('Learn React')).toBeInTheDocument();
@@ -16,6 +17,8 @@ describe('TodoList Component', () => {
 
     fireEvent.change(input, { target: { value: 'Test Todo' } });
     fireEvent.click(button);
+    
+    
     expect(screen.getByText('Test Todo')).toBeInTheDocument();
   });
 
@@ -23,14 +26,18 @@ describe('TodoList Component', () => {
     render(<TodoList />);
     const todo = screen.getByText('Learn React');
     fireEvent.click(todo);  
-    expect(todo).toHaveStyle('text-decoration: line-through');  
+    
+
+    expect(todo).toHaveStyle('text-decoration: line-through');
   });
 
   test('deletes a todo', () => {
     render(<TodoList />);
     const todo = screen.getByText('Learn React');
-    const deleteButton = screen.getAllByText('Delete')[0];  
-    fireEvent.click(deleteButton);  
-    expect(todo).not.toBeInTheDocument();  
+    const deleteButton = screen.getAllByText('Delete')[0]; 
+    fireEvent.click(deleteButton);
+    
+   
+    expect(todo).not.toBeInTheDocument();
   });
 });
