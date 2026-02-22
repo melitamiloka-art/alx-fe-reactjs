26fb9e285e84cdcc1040b9d0f2fd2da4eeb2bd31
Melita <melitamiloka@gmail.com>
2026-02-22 17:15:33 +0100

    Add react-todo

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import TodoList from '../components/TodoList';  
 import React, { useState } from 'react';
