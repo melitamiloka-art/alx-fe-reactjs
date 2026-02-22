41edea0515a0f398e7c5ebf1a6b4be3071012cb6
Melita <melitamiloka@gmail.com>
2026-02-22 12:06:01 +0100

    Add react-router-advanced

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileDetails = () => {
+  return <h3>This is the Profile Details page</h3>;
+};
+
+export default ProfileDetails;
---
+++ react-router-advanced/src/components/ProfileSetting.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileSettings = () => {
+  return <h3>This is the Profile Settings page</h3>;
+};
+
+export default ProfileSettings;
